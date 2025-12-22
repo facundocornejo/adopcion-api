b4878f322d1a2c610a4fea897c648606ec368803
facundo cornejo <facundocornejodediego@hotmail.com>
2025-12-22 19:48:33 -0300

    fix: Corregir nombre de archivo auth.middleware.js

--- src/routes/casosexito.routes.js
+++ src/routes/casosexito.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verificarToken } = require('../middlewares/auth');
+const { verificarToken } = require('../middlewares/auth.middleware');
 const {
   getCasosExito,
   getCasosExitoByOrg,
